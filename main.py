004a3d8860e87b798f5c3f838bc1714287d083ac
DanielAldas <54075272+DanielAldas@users.noreply.github.com>
2022-05-16 18:28:08 -0500

    Add files via upload
    enemies assets added

--- main.py
+++ main.py
@@ -2,32 +2,33 @@ import pygame
 import os
 import time
 import random
+
 pygame.font.init()
 pygame.init()
 
-WIDTH, HEIGHT = 750, 750
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+largo = 900
+alto = 750
+pantalla = pygame.display.set_mode((largo, alto))
 pygame.display.set_caption("Space Benito")
+
+#cargando assets
+# Personajes
+kendrick = pygame.image.load("assets/kendrick.png")
+jackh = pygame.image.load("assets/jack.png")
+rosalia = pygame.image.load("assets/rosalia.png")
+badbunny =  pygame.image.load("assets/benito.png")
+
+# Disparos
+mrmorale = pygame.image.load("assets/morale.jpeg")
+comeh = pygame.image.load("assets/harlow.jpeg")
+motomami = pygame.image.load("assets/motomami.jpg")
+verano = pygame.image.load("assets/verano.jpg")
 sonido_disparo=pygame.mixer.Sound("assets/yeh.wav")
 
-# Load images
-RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "grammy.png"))
-GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "vma.png"))
-BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "ama.png"))
+#Fondo de pantalla
+fondo = pygame.transform.scale(pygame.image.load("assets/fondo.png"), (largo, alto))
 
-# Player player
-YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "benito.png"))
-
-# Lasers
-RED_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
-GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
-BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
-YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
-
-# Background
-BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "fondo.png")), (WIDTH, HEIGHT))
-
-class Laser:
+class Album:
     def __init__(self, x, y, img):
         self.x = x
         self.y = y
@@ -37,42 +38,43 @@ class Laser:
     def draw(self, window):
         window.blit(self.img, (self.x, self.y))
 
-    def move(self, vel):
+    def mover(self, vel):
         self.y += vel
 
     def off_screen(self, height):
         return not(self.y <= height and self.y >= 0)
 
     def collision(self, obj):
-        return collide(self, obj)
+        return colide(self, obj)
 
 
-class Ship:
+class Personaje:
+
     COOLDOWN = 30
 
-    def __init__(self, x, y, health=100):
+    def __init__(self, x, y, vida=100):
         self.x = x
         self.y = y
-        self.health = health
-        self.ship_img = None
-        self.laser_img = None
-        self.lasers = []
+        self.vida = vida
+        self.personaje_img = None
+        self.album_img = None
+        self.albums = []
         self.cool_down_counter = 0
 
     def draw(self, window):
-        window.blit(self.ship_img, (self.x, self.y))
-        for laser in self.lasers:
-            laser.draw(window)
+        window.blit(self.personaje_img, (self.x, self.y))
+        for album in self.albums:
+            album.draw(window)
 
-    def move_lasers(self, vel, obj):
+    def mover_album(self, vel, obj):
         self.cooldown()
-        for laser in self.lasers:
-            laser.move(vel)
-            if laser.off_screen(HEIGHT):
-                self.lasers.remove(laser)
-            elif laser.collision(obj):
-                obj.health -= 10
-                self.lasers.remove(laser)
+        for album in self.albums:
+            album.mover(vel)
+            if album.off_screen(alto):
+                self.albums.remove(album)
+            elif album.collision(obj):
+                obj.vida -= 10
+                self.albums.remove(album)
 
     def cooldown(self):
         if self.cool_down_counter >= self.COOLDOWN:
@@ -82,79 +84,80 @@ class Ship:
 
     def shoot(self):
         if self.cool_down_counter == 0:
-            laser = Laser(self.x, self.y, self.laser_img)
-            self.lasers.append(laser)
+            album = Album(self.x, self.y, self.album_img)
+            self.albums.append(album)
             self.cool_down_counter = 1
 
     def get_width(self):
-        return self.ship_img.get_width()
+        return self.personaje_img.get_width()
 
     def get_height(self):
-        return self.ship_img.get_height()
+        return self.personaje_img.get_height()
 
 
-class Player(Ship):
-    def __init__(self, x, y, health=100):
-        super().__init__(x, y, health)
-        self.ship_img = YELLOW_SPACE_SHIP
-        self.laser_img = YELLOW_LASER
-        self.mask = pygame.mask.from_surface(self.ship_img)
-        self.max_health = health
+class Jugador(Personaje):
+    def __init__(self, x, y, vida=100):
+        super().__init__(x, y, vida)
+        self.personaje_img = badbunny
+        self.album_img = verano
+        self.mask = pygame.mask.from_surface(self.personaje_img)
+        self.vida_max = vida
 
-    def move_lasers(self, vel, objs):
+    def mover_album(self, vel, objs):
         self.cooldown()
-        for laser in self.lasers:
-            laser.move(vel)
-            if laser.off_screen(HEIGHT):
-                self.lasers.remove(laser)
+        for album in self.albums:
+            album.mover(vel)
+            if album.off_screen(largo):
+                self.albums.remove(album)
             else:
                 for obj in objs:
-                    if laser.collision(obj):
+                    if album.collision(obj):
                         objs.remove(obj)
-                        if laser in self.lasers:
-                            self.lasers.remove(laser)
+                        if album in self.albums:
+                            self.albums.remove(album)
 
     def draw(self, window):
         super().draw(window)
         self.healthbar(window)
 
     def healthbar(self, window):
-        pygame.draw.rect(window, (255,0,0), (self.x, self.y + self.ship_img.get_height() + 10, self.ship_img.get_width(), 10))
-        pygame.draw.rect(window, (0,255,0), (self.x, self.y + self.ship_img.get_height() + 10, self.ship_img.get_width() * (self.health/self.max_health), 10))
+        pygame.draw.rect(window, (255,0,0), (self.x, self.y + self.personaje_img.get_height() + 10, self.personaje_img.get_width(), 10))
+        pygame.draw.rect(window, (0,255,0), (self.x, self.y + self.personaje_img.get_height() + 10, self.personaje_img.get_width() * (self.vida/self.vida_max), 10))
 
 
-class Enemy(Ship):
-    COLOR_MAP = {
-                "red": (RED_SPACE_SHIP, RED_LASER),
-                "green": (GREEN_SPACE_SHIP, GREEN_LASER),
-                "blue": (BLUE_SPACE_SHIP, BLUE_LASER)
-                }
+class Enemigo(Personaje):
+    mapa_nombres = { 
+        "rosalia" : (rosalia, motomami),
+        "jack" : (jackh, comeh),
+        "kendrick" : (kendrick, mrmorale)
+        }
 
-    def __init__(self, x, y, color, health=100):
-        super().__init__(x, y, health)
-        self.ship_img, self.laser_img = self.COLOR_MAP[color]
-        self.mask = pygame.mask.from_surface(self.ship_img)
+    def __init__(self, x, y, nombre, vida=100):
+        super().__init__(x, y, vida)
+        self.personaje_img, self.album_img = self.mapa_nombres[nombre]
+        self.mask = pygame.mask.from_surface(self.personaje_img)
 
     def move(self, vel):
         self.y += vel
 
     def shoot(self):
         if self.cool_down_counter == 0:
-            laser = Laser(self.x-20, self.y, self.laser_img)
-            self.lasers.append(laser)
+            album = Album(self.x + 15, self.y, self.album_img)
+            self.albums.append(album)
             self.cool_down_counter = 1
 
 
-def collide(obj1, obj2):
+def colide(obj1, obj2):
     offset_x = obj2.x - obj1.x
     offset_y = obj2.y - obj1.y
     return obj1.mask.overlap(obj2.mask, (offset_x, offset_y)) != None
 
 def main():
     run = True
-    FPS = 60
+    fps = 60
     level = 0
-    lives = 5
+    crash = 5
+    score = 0
     main_font = pygame.font.SysFont("comicsans", 50)
     lost_font = pygame.font.SysFont("comicsans", 60)
     
@@ -163,12 +166,12 @@ def main():
     
     enemies = []
     wave_length = 5
-    enemy_vel = 1
+    enemigo_vel = 1
 
     player_vel = 5
-    laser_vel = 5
+    album_vel = 5
 
-    player = Player(300, 630)
+    benito = Jugador(300, 630)
 
     clock = pygame.time.Clock()
 
@@ -176,39 +179,39 @@ def main():
     lost_count = 0
 
     def redraw_window():
-        WIN.blit(BG, (0,0))
+        pantalla.blit(fondo, (0,0))
         # draw text
-        lives_label = main_font.render(f"Vidas: {lives}", 1, (255,255,255))
+        score_label = main_font.render(f"Choques: {crash}", 1, (255,255,255))
         level_label = main_font.render(f"Nivel: {level}", 1, (255,255,255))
 
-        WIN.blit(lives_label, (10, 10))
-        WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+        pantalla.blit(score_label, (10, 10))
+        pantalla.blit(level_label, (largo - level_label.get_width() - 10, 10))
 
-        for enemy in enemies:
-            enemy.draw(WIN)
+        for enemigo in enemies:
+            enemigo.draw(pantalla)
 
-        player.draw(WIN)
+        benito.draw(pantalla)
 
         if lost:
             pygame.mixer.music.stop()
             pygame.mixer.music.load('assets/si_veo_a_tu_mama.wav')
             pygame.mixer.music.play(-1, 0.0)
-            lost_label = lost_font.render("pelas", 1, (255,255,255))
-            WIN.blit(lost_label, (WIDTH/2 - lost_label.get_width()/2, 350))
+            lost_label = lost_font.render("Perdiste!!", 1, (255,255,255))
+            pantalla.blit(lost_label, (largo/2 - lost_label.get_width()/2, 350))
             
 
         pygame.display.update()
 
     while run:
-        clock.tick(FPS)
+        clock.tick(fps)
         redraw_window()
 
-        if lives <= 0 or player.health <= 0:
+        if crash <= 0 or benito.vida <= 0:
             lost = True
             lost_count += 1
 
         if lost:
-            if lost_count > FPS * 3:
+            if lost_count > fps * 3:
                 run = False
             else:
                 continue
@@ -217,51 +220,54 @@ def main():
             level += 1
             wave_length += 5
             for i in range(wave_length):
-                enemy = Enemy(random.randrange(50, WIDTH-100), random.randrange(-1500, -100), random.choice(["red", "blue", "green"]))
-                enemies.append(enemy)
+                enemigo = Enemigo(random.randrange(50, largo-100), random.randrange(-1500, -100), random.choice(["kendrick", "rosalia", "jack"]))
+                enemies.append(enemigo)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quit()
 
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_a] and player.x - player_vel > 0: # left
-            player.x -= player_vel
-        if keys[pygame.K_d] and player.x + player_vel + player.get_width() < WIDTH: # right
-            player.x += player_vel
-        if keys[pygame.K_w] and player.y - player_vel > 0: # up
-            player.y -= player_vel
-        if keys[pygame.K_s] and player.y + player_vel + player.get_height() + 15 < HEIGHT: # down
-            player.y += player_vel
+        if keys[pygame.K_a] or keys[pygame.K_LEFT] and benito.x - player_vel > 0: # left
+            benito.x -= player_vel
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT] and benito.x + player_vel + benito.get_width() < largo: # right
+            benito.x += player_vel
+        if keys[pygame.K_w] or keys[pygame.K_UP]and benito.y - player_vel > 0: # up
+            benito.y -= player_vel
+        if keys[pygame.K_s] or keys[pygame.K_DOWN] and benito.y + player_vel + benito.get_height() + 15 < largo: # down
+            benito.y += player_vel
         if keys[pygame.K_SPACE]:
-            player.shoot()
             sonido_disparo.play()
+            benito.shoot()
+            
 
-        for enemy in enemies[:]:
-            enemy.move(enemy_vel)
-            enemy.move_lasers(laser_vel, player)
+        for enemigo in enemies[:]:
+            enemigo.move(enemigo_vel)
+            enemigo.mover_album(album_vel, benito)
 
             if random.randrange(0, 2*60) == 1:
-                enemy.shoot()
+                enemigo.shoot()
 
-            if collide(enemy, player):
-                player.health -= 10
-                enemies.remove(enemy)
-            elif enemy.y + enemy.get_height() > HEIGHT:
-                lives -= 1
-                enemies.remove(enemy)
+            if colide(enemigo, benito):
+                benito.vida -= 10
+                score += 1
+                enemies.remove(enemigo)
+            elif enemigo.y + enemigo.get_height() > largo:
+                crash -= 1
+                enemies.remove(enemigo)
+                
 
-        player.move_lasers(-laser_vel, enemies)
+        benito.mover_album(-album_vel, enemies)
 
 def main_menu():
-    pygame.mixer.music.load('assets/si_veo_a_tu_mama.wav')
+    pygame.mixer.music.load('assets/trellas.mp3')
     pygame.mixer.music.play(-1, 0.0)
     title_font = pygame.font.SysFont("comicsans", 70)
     run = True
     while run:
-        WIN.blit(BG, (0,0))
+        pantalla.blit(fondo, (0,0))
         title_label = title_font.render("Click para jugar", 1, (255,255,255))
-        WIN.blit(title_label, (WIDTH/2 - title_label.get_width()/2, 350))
+        pantalla.blit(title_label, (largo/2 - title_label.get_width()/2, 350))
         pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
